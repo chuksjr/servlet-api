a00fd11c5e9f0f8312f55abd64dc2d8ab444b2f3
Chuks Ricardo <C0628005@vale.com>
2025-05-25 23:59:52 -0300

    init

---
+++ servlet-api/src/test/java/br/com/chuksricardo/servlet_api/ServletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chuksricardo.servlet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
